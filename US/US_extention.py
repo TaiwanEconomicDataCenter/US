1b3c398b5ca8f5484bb9cd030606c75ad3251204
TaiwanEconomicDataCenter <tedc1@ms12.hinet.net>
2022-04-25 11:40:26 +0800

    cd

--- US/US_extention.py
+++ US/US_extention.py
@@ -31,7 +31,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime, date, timedelta
 from dateutil.relativedelta import relativedelta
 from urllib.error import HTTPError
-sys.path.append('../TO_DB')
+sys.path.append('../../TO_DB/TO_DB')
 from TO_DB import SELECT_DF_KEY, SELECT_DATABASES, INSERT_TABLES
 urllib3.disable_warnings()
 warnings.simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
