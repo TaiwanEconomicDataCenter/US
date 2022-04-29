e3d3d06ab48e52e77d553563fbba7b1a92f6c3c7
TaiwanEconomicDataCenter <tedc1@ms12.hinet.net>
2022-04-29 15:33:21 +0800

    Update US_extention.py

--- US/US_extention.py
+++ US/US_extention.py
@@ -1095,6 +1095,7 @@ def UPDATE(original_file, updated_file, key_list, NAME, data_path, orig_suf, up_
     original_file = original_file.reset_index()
     original_file = original_file.reindex(key_list, axis='columns')
     logging.info('updated: '+str(updated)+'\n')
+    logging.info('Time: '+str(int(time.time() - tStart))+' s'+'\n')
 
     return original_file, original_database
 
